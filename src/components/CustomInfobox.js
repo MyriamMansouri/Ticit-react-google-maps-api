56acba3895abb91259af403c309c4af9e7f32115
myriam mansouri <myriam.mansouri@gmail.com>
2020-09-11 13:41:26 -0400

    Changed cloudinary credentials

--- src/components/CustomInfobox.js
+++ src/components/CustomInfobox.js
@@ -110,7 +110,7 @@ const CustomInfobox = ({ marker }) => {
     >
       <SmallCard>
         {imgId && (
-          <Image  style={{backgroundColor:COLORS.lightText}} width="240" height="240" cloudName="hcrafbjaa" publicId={imgId}>
+          <Image  style={{backgroundColor:COLORS.lightText}} width="240" height="240" cloudName="hmaw0pqml" publicId={imgId}>
             <Transformation width="240" height="240" crop="fill" quality="auto:low" />
           </Image>
         )}
